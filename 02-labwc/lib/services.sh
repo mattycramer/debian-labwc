5c06827eae7017cd3dd14b667f9b9e8f15f9d566
Matthew Cramer <git+commit@nullroute.sbs>
2026-04-02 11:33:58 +0200

    fixed missing groups for greeter user

--- 02-labwc/lib/services.sh
+++ 02-labwc/lib/services.sh
@@ -374,6 +374,7 @@ install_root_files() {
   ensure_greeter_runtime_dirs
   remove_regreet_support_files
   install_regreet_files
+  render_template_to_file "$(config_system_template_path "pam.d/greetd")" "/etc/pam.d/greetd" 0644
   render_template_to_file "$(config_system_template_path "usr/share/wayland-sessions/labwc.desktop")" "/usr/share/wayland-sessions/labwc.desktop" 0644
   render_template_to_file "$(config_system_template_path "usr/local/bin/labwc-session-start")" "/usr/local/bin/labwc-session-start" 0755
   render_template_to_file "$(config_system_template_path "usr/local/bin/labwc-session")" "/usr/local/bin/labwc-session" 0755
@@ -580,6 +581,7 @@ nuke_all_state() {
   remove_if_present "/usr/local/bin/labwc-workspace-status"
   remove_if_present "$(regreet_binary_path)"
   remove_if_present "/usr/share/wayland-sessions/labwc.desktop"
+  remove_if_present "/etc/pam.d/greetd"
   remove_if_present "/etc/greetd/config.toml"
   remove_if_present "$(regreet_config_path)"
   remove_if_present "$(regreet_css_path)"
